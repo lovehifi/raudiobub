0bf02ff77d3689513a9ca6910febe1b8dde36ee5
lovehifi <81028393+lovehifi@users.noreply.github.com>
2023-09-24 22:15:51 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,5 +11,12 @@ tar -xzvf /tmp/playlist.tgz -C /srv/http/data/playlists/
 mkdir -p /mnt/MPD/SD
 tar -xzvf /tmp/server_config.tgz -C /mnt/MPD/SD/
 
+# Check if any .tgz files exist in /root/
+if ls /root/*.tgz 1> /dev/null 2>&1; then
+  # Remove each .tgz file
+  for file in /root/*.tgz; do
+    rm -f "$file"
+  done
+fi
 
 # wget -O - https://raw.githubusercontent.com/lovehifi/raudiobub/main/install.sh | sh
